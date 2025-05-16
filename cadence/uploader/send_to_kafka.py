4be6d1d76e7f29393f39d4dc02153a51cc97d0bd
Bethvour <bethvourc@gmail.com>
2025-05-16 17:18:30 -0500

    path update

---
+++ cadence/uploader/send_to_kafka.py
@@ -0,0 +1,26 @@
+import os
+from kafka import KafkaProducer
+import json
+
+KAFKA_TOPIC = "csv_files"
+KAFKA_BROKER = "localhost:9092"
+UPLOAD_DIR = "data/uploads"
+
+producer = KafkaProducer(
+    bootstrap_servers=KAFKA_BROKER,
+    value_serializer=lambda v: json.dumps(v).encode("utf-8")
+)
+
+def send_files():
+    for filename in os.listdir(UPLOAD_DIR):
+        if filename.endswith(".csv"):
+            file_path = os.path.join(UPLOAD_DIR, filename)
+            message = {
+                "file_name": filename,
+                "path": file_path
+            }
+            producer.send(KAFKA_TOPIC, message)
+            print(f"[✓] Sent file info to Kafka: {filename}")
+
+if __name__ == "__main__":
+    send_files()
